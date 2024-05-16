0fe58038ca230174958f0cab56faf3f46651f358
Denys Borysiuk <denisborisiuk@gmail.com>
2024-05-16 21:39:43 +0300

    fix schemas

--- schemas/events.js
+++ schemas/events.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const updateParticipantsSchema = Joi.object({
-  name: Joi.string().required().min(1),
+  fullName: Joi.string().required().min(1),
   email: Joi.string().required(),
   dateOfBirth: Joi.string().required(),
   about: Joi.string().required(),
